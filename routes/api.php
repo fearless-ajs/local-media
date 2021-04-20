eceefceeb8ea06dcd7a7fcb0b7bf6875acb966a2
kodejuice <kodejuice@gmail.com>
2021-04-20 12:14:07 +0100

    unprotect routes

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,31 @@ Route::group(['middleware' => ['cors', 'json.response', 'throttle:60,1']], funct
     // auth
     Route::post('login', [AuthController::class, 'login']);
 
+    /////////////////////
+    // MediaController //
+    /////////////////////
+    Route::get('media/all', [App\Http\Controllers\API\MediaController::class, 'getAllMedia'])->name('all_media');
+    Route::get('pdf/latest', [App\Http\Controllers\API\MediaController::class, 'getLatestPDF'])->name('latest_pdf');
+    Route::get('media/{id}', [App\Http\Controllers\API\MediaController::class, 'getMedia'])->name('get_media');
+    Route::get('media/{id}/comments', [App\Http\Controllers\API\MediaController::class, 'getMediaComments'])->name('get_media_comments');
+    Route::get('media/{id}/export', [App\Http\Controllers\API\AnalyticsController::class, 'exportStats'])->name('export_stats');
+    // 
+    Route::post('media', [App\Http\Controllers\API\MediaController::class, 'uploadMedia'])->name('upload_media');
+    //
+    Route::post('media/{id}/like', [App\Http\Controllers\API\MediaController::class, 'like'])->name('like_media');
+    Route::post('media/{id}/share', [App\Http\Controllers\API\MediaController::class, 'share'])->name('share_media');
+    Route::post('media/{id}/comment', [App\Http\Controllers\API\MediaController::class, 'comment'])->name('comment');
+    Route::post('media/{media_id}/{user_id}/view', [App\Http\Controllers\API\MediaController::class, 'view'])->name('view_media');
+    // 
+    Route::post('media/{id}/open', [App\Http\Controllers\API\MediaController::class, 'media_open'])->name('media_open');
+    Route::post('media/{id}/bounce', [App\Http\Controllers\API\MediaController::class, 'media_bounce'])->name('media_bounce');
+    Route::post('media/{id}/engage', [App\Http\Controllers\API\MediaController::class, 'media_engage'])->name('media_engage');
+    //
+    Route::get('media/{id}/distributors', [App\Http\Controllers\API\AnalyticsController::class, 'getDistributors'])->name('get_distributors');
+    // 
+    Route::delete('media/{id}', [App\Http\Controllers\API\MediaController::class, 'deleteMedia'])->name('delete_media');
+
+
     // protected routes
     Route::group(['middleware' => 'auth'], function () {
 
@@ -30,30 +55,6 @@ Route::group(['middleware' => ['cors', 'json.response', 'throttle:60,1']], funct
         Route::get('users', [App\Http\Controllers\API\UserController::class, 'getUsers'])->name('all_users');
         Route::post('user', [App\Http\Controllers\API\UserController::class, 'addUser'])->name('add_user');
         Route::delete('user/{id}', [App\Http\Controllers\API\UserController::class, 'deleteUser'])->name('delete_user');
-
-        /////////////////////
-        // MediaController //
-        /////////////////////
-        Route::get('media/all', [App\Http\Controllers\API\MediaController::class, 'getAllMedia'])->name('all_media');
-        Route::get('pdf/latest', [App\Http\Controllers\API\MediaController::class, 'getLatestPDF'])->name('latest_pdf');
-        Route::get('media/{id}', [App\Http\Controllers\API\MediaController::class, 'getMedia'])->name('get_media');
-        Route::get('media/{id}/comments', [App\Http\Controllers\API\MediaController::class, 'getMediaComments'])->name('get_media_comments');
-        Route::get('media/{id}/export', [App\Http\Controllers\API\AnalyticsController::class, 'exportStats'])->name('export_stats');
-        // 
-        Route::post('media', [App\Http\Controllers\API\MediaController::class, 'uploadMedia'])->name('upload_media');
-        //
-        Route::post('media/{id}/like', [App\Http\Controllers\API\MediaController::class, 'like'])->name('like_media');
-        Route::post('media/{id}/share', [App\Http\Controllers\API\MediaController::class, 'share'])->name('share_media');
-        Route::post('media/{id}/comment', [App\Http\Controllers\API\MediaController::class, 'comment'])->name('comment');
-        Route::post('media/{media_id}/{user_id}/view', [App\Http\Controllers\API\MediaController::class, 'view'])->name('view_media');
-        // 
-        Route::post('media/{id}/open', [App\Http\Controllers\API\MediaController::class, 'media_open'])->name('media_open');
-        Route::post('media/{id}/bounce', [App\Http\Controllers\API\MediaController::class, 'media_bounce'])->name('media_bounce');
-        Route::post('media/{id}/engage', [App\Http\Controllers\API\MediaController::class, 'media_engage'])->name('media_engage');
-        //
-        Route::get('media/{id}/distributors', [App\Http\Controllers\API\AnalyticsController::class, 'getDistributors'])->name('get_distributors');
-        // 
-        Route::delete('media/{id}', [App\Http\Controllers\API\MediaController::class, 'deleteMedia'])->name('delete_media');
 
 
         /////////////////////////
